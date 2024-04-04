667425f123edbb9b0fcb4de7ce60c0c668468740
KaurGR <kaur-gabriel.rajasalu@tktk.ee>
2024-04-04 11:11:15 +0300

    Add week2

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Application asks user age
+if (age<13) output "grow up" 
+else if (age>13) output "welcome to our application"
+*/
+
+Console.WriteLine("Enter your age:");
+//int age = Int32.Parse(Console.ReadLine());
+
+string age = Console.ReadLine();
+int ageNum = 0;
+
+bool isAgeNum = Int32.TryParse(age, out ageNum);
+
+if (isAgeNum)
+{
+    if (ageNum < 13)
+    {
+        Console.WriteLine("Grow up!");
+    }
+    else // if(age >= 13)
+    {
+        Console.WriteLine("welcome to our application");
+    }
+}
+else
+{
+    Console.WriteLine("Enter numbers");
+}
---
+++ UserGender/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Application asks user gender (M/F)
+Application asks for name
+Application greets user "Welcome, [Mr./Ms.] (name)!"
+*/
+
+Console.WriteLine("Input your gender (M/F):");
+char userGender = Char.Parse(Console.ReadLine()); //single char saved as string
+Console.WriteLine("Please enter your name:");
+string name = Console.ReadLine();
+
+if (userGender == 'M')
+{
+    Console.WriteLine($"Welcome, Mr. {name}");
+}
+else if (userGender == 'F')
+{
+    Console.WriteLine($"Welcome, Mr. {name}");
+}
+else
+{
+    Console.WriteLine($"Hello, {name}");
+}
